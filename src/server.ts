dbd0f70e3d20c3c8e6400359d1ee462aeebe2da0
Dami Adefeko <damiadefeko@yahoo.com>
2025-05-12 19:26:14 +0100

    connect database

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,25 @@ import express from 'express';
 import config from './config/config';
 import morgan from 'morgan';
 import { errorHandler } from './middleware/errorHandler';
+import mongoose from 'mongoose';
 
-const app = express();
+(async () => {
+    await mongoose.connect(config.dbUrl);
 
-// Middleware
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    const app = express();
 
-// Routes
-app.use('/', (req, res) => {
-    res.send('Hello, World!');
-});
-app.use(errorHandler);
+    // Middleware
+    app.use(morgan('dev'));
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
 
-app.listen(config.port, () => {
-    console.log(`Server is running on port ${config.port}`);
-}
-);
+    // Routes
+    app.use('/', (req, res) => {
+        res.send('Hello, World!');
+    });
+    app.use(errorHandler);
+
+    app.listen(config.port, () => {
+        console.log(`Server is running on port ${config.port}`);
+    });
+})();
