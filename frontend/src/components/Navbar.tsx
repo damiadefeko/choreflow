f8b4d80cccfc95159bfbf0a71d5a8c3abadfbd94
Dami Adefeko <damiadefeko@yahoo.com>
2025-06-16 21:14:52 +0100

    fix nav styling and added link back to home page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,17 +1,21 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/images/logo.png';
 export function Navbar() {
-    const navLinkStyle = 'text-white font-medium text-[14px] md:text-[16px] xl:text-[20px]';
+    const navLinkStyle = 'text-white hover:text-(--primary-300) font-medium text-[14px] md:text-[16px] xl:text-[20px]';
     return (
         <nav className='w-full flex justify-between px-[24px] md:px-[74px] xl:px-[88px] bg-(--primary-100) items-center'>
-            <img src={logo} className='w-[100px] md:w-[120px]' />
+            <NavLink to='/'>
+                <img src={logo} className='w-[100px] md:w-[120px]' />
+            </NavLink>
             <div className='flex gap-6'>
-                <a href='#' className={navLinkStyle}>
+                <NavLink
+                    to='/login'
+                    className={({ isActive }) => `${navLinkStyle} ${isActive ? '!text-(--primary-300)' : ''}`}>
                     Login
-                </a>
+                </NavLink>
                 <NavLink
                     to='/register'
-                    className={({ isActive }) => `${navLinkStyle} ${isActive ? 'text-(--primary-200)' : ''}`}>
+                    className={({ isActive }) => `${navLinkStyle} ${isActive ? '!text-(--primary-300)' : ''}`}>
                     Register
                 </NavLink>
             </div>
